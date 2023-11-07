b13b68056ba201b33111e20ce524dade5793e6e3
SWYZYZ <1617220899@qq.com>
2023-11-07 15:22:26 +0800

    update

--- index.js
+++ index.js
@@ -32,15 +32,11 @@ router.post("/api/msgt", async (ctx) => {
   // console.log(ctx)
   // console.log(ctx.headers)
   const headers = ctx.headers
-  const weixinAPI = "http://api.weixin.qq.com/cgi-bin/message/custom/send"
-  console.log(headers['x-wx-openid'])
-  console.log(JSON.stringify(ctx.body, null, 2))
 
   let a = await client.get("https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=wx3fbbaa0b94922ed1&secret=45bd3b7671f2ce27d383f2b41cd7eae9")
   let access_token = a.access_token
-
+  const weixinAPI = "http://api.weixin.qq.com/cgi-bin/message/custom/send?access_token="+access_token
   const payload = {
-    access_token:access_token,
     touser: headers['x-wx-openid'],
     msgtype: 'text',
     text: {
@@ -49,7 +45,6 @@ router.post("/api/msgt", async (ctx) => {
   }
   // dispatch to wx server
   const result = await client.post(weixinAPI, payload)
-  console.log(result)
 
   ctx.body = {
     code: 0,
