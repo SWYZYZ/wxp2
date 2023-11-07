6b6ebd47576569b708da354d779a193fce553321
SWYZYZ <1617220899@qq.com>
2023-11-07 13:02:07 +0800

    update

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ router.post("/api/msgt", async (ctx) => {
         touser: headers['x-wx-openid'],
         msgtype: 'text',
         text: {
-            content: `云托管接收消息推送成功，内容如下：\n${JSON.stringify(req.body, null, 2)}`
+            content: `云托管接收消息推送成功，内容如下：\n${JSON.stringify(ctx.body, null, 2)}`
         }
     }
     // dispatch to wx server
