baffd551e917d61fa1fcd45d8424a6a75493a7bb
SWYZYZ <1617220899@qq.com>
2023-11-07 13:12:07 +0800

    update

--- index.js
+++ index.js
@@ -39,7 +39,8 @@ router.post("/api/msgt", async (ctx) => {
         }
     }
     // dispatch to wx server
-    const result = await client.post(weixinAPI, payload)
+    const result = router.post(weixinAPI,payload)
+    console.log(result)
   // const { request } = ctx;
   // const weixinAPI = `http://api.weixin.qq.com/cgi-bin/message/custom/send`
   // const payload = {
