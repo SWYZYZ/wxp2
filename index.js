14177ae130cdc225572dfef0b2628c533030153c
SWYZYZ <1617220899@qq.com>
2023-11-07 12:30:32 +0800

    update

--- index.js
+++ index.js
@@ -21,18 +21,28 @@ router.post("/api/count", async (ctx) => {
   console.log(ctx)
   ctx.body = {
     code: 0,
-    data: "消息为："+ctx,
+    data: "消息为：" + ctx,
   };
 });
 
 // 测试接收
 router.post("/api/msgt", async (ctx) => {
-  const { request } = ctx;
   console.log(ctx)
-
+  const { request } = ctx;
+  // const weixinAPI = `http://api.weixin.qq.com/cgi-bin/message/custom/send`
+  // const payload = {
+  //   touser: headers['x-wx-openid'],
+  //   msgtype: 'text',
+  //   text: {
+  //     content: `云托管接收消息推送成功，内容如下：\n${JSON.stringify(req.body, null, 2)}`
+  //   }
+  // }
+  // dispatch to wx server
+  const result = await client.post(weixinAPI, payload)
+  console.log('received request', req.body, result.data)
   ctx.body = {
     code: 0,
-    data: "收到消息："+ctx,
+    data: "收到消息：" + ctx,
   };
 });
 
